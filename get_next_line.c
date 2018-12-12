99ff496c1ab4934ee1f6047e828f1579175a2730
Axel GERVEAU <axelgerv@e3r9p7.42.fr>
2018-12-12 17:55:29 +0100

    leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: axelgerv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 18:32:48 by axelgerv          #+#    #+#             */
-/*   Updated: 2018/12/10 18:55:21 by axelgerv         ###   ########.fr       */
+/*   Updated: 2018/12/12 17:48:10 by axelgerv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,30 @@ t_data	*ft_add_list(int fd)
 
 t_data	*ft_check_fd(int fd, t_data *element)
 {
+	t_data	*new;
+	t_data	*head;
+
+	head = element;
 	while (element)
 	{
-		if (fd == element->fd)
+		if (element->fd == fd)
 			return (element);
 		element = element->next;
 	}
-	return (ft_add_list(fd));
+	new = ft_add_list(fd);
+	if (!(element))
+		element = new;
+	else
+	{
+		while (element->next)
+			element = element->next;
+		element->next = new;
+	}
+	element = head;
+	return (new);
 }
 
-char	*ft_storage(t_data *element, char **line)
+int		ft_storage(t_data *element, char **line)
 {
 	int		i;
 	char	*rest;
@@ -44,36 +58,34 @@ char	*ft_storage(t_data *element, char **line)
 	rest = ft_strchr(element->str, '\n');
 	if (rest)
 		*rest = '\0';
-	*line = ft_strdup(element->str);
+	if (!(*line = ft_strdup(element->str)))
+		return (-1);
 	if (rest)
-		element->str = ft_strdup(rest + 1);
-	return (*line);
+	{
+		if (!(element->str = ft_strdup(rest + 1)))
+			return (-1);
+	}
+	else
+		return (0);
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	static	t_data	*element;
-	char			buf[BUFF_SIZE + 1];
+	char			*buf;
 	int				rd;
-	int				i;
 
-	i = 0;
 	element = ft_check_fd(fd, element);
-	rd = 1;
-	if (!fd || !line)
+	if (fd < 0 || !line || !(buf = ft_strnew(BUFF_SIZE)))
 		return (-1);
-	while ((rd > 0) && (!element->str ||
-				(element->str && (!ft_strchr(element->str, '\n')))))
+	while ((rd = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		rd = read(fd, &buf, BUFF_SIZE);
-		if (element->str == NULL)
-			element->str = ft_strdup(buf);
-		else
-			element->str = ft_strjoin(element->str, buf);
-		ft_strclr(buf);
+		(element->str = ft_strjoinf(&(element->str),
+									&buf, element->str ? 1 : 0));
+		if (ft_strchr(element->str, '\n'))
+			break ;
 	}
-	ft_storage(element, line);
-	ft_putstr("Ca c'est element->str : ");
-	ft_putstr(element->str);
-	return (0);
+	ft_strdel(&buf);
+	return (ft_storage(element, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: axelgerv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 18:33:37 by axelgerv          #+#    #+#             */
-/*   Updated: 2018/12/10 18:58:22 by axelgerv         ###   ########.fr       */
+/*   Updated: 2018/12/12 17:47:08 by axelgerv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 5
+# define BUFF_SIZE 1
 
 # include <sys/types.h>
 # include <sys/uio.h>
@@ -24,13 +24,10 @@
 typedef	struct		s_data
 {
 	char			*str;
-	char			*tmp;
 	int				fd;
 	struct s_data	*next;
 }					t_data;
 
-int					check_ifexist(t_data *list, int fd);
-t_data				*ft_add_list(int fd);
 int					get_next_line(const int fd, char **line);
 
 #endif
