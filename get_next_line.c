cfeab82e4b91e821cc1cb186d7a847a7104fedde
Axel GERVEAU <axelgerv@e3r12p9.42.fr>
2018-12-13 19:52:54 +0100

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: axelgerv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 18:32:48 by axelgerv          #+#    #+#             */
-/*   Updated: 2018/12/12 17:48:10 by axelgerv         ###   ########.fr       */
+/*   Updated: 2018/12/13 19:50:08 by axelgerv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-t_data	*ft_add_list(int fd)
+t_data	*ft_new_list(int fd)
 {
 	t_data *new;
 
@@ -26,66 +27,52 @@ t_data	*ft_add_list(int fd)
 
 t_data	*ft_check_fd(int fd, t_data *element)
 {
-	t_data	*new;
-	t_data	*head;
+	t_data *new;
 
-	head = element;
-	while (element)
-	{
-		if (element->fd == fd)
-			return (element);
-		element = element->next;
-	}
-	new = ft_add_list(fd);
-	if (!(element))
-		element = new;
-	else
-	{
-		while (element->next)
-			element = element->next;
-		element->next = new;
-	}
-	element = head;
+	if (element->next && element->fd != fd)
+		return (ft_check_fd(fd, element->next));
+	if (element->fd == fd)
+		return (element);
+	new = ft_new_list(fd);
+	element->next = new;
 	return (new);
 }
 
 int		ft_storage(t_data *element, char **line)
 {
-	int		i;
 	char	*rest;
 
-	i = 0;
-	rest = ft_strchr(element->str, '\n');
-	if (rest)
+	if (!element->str || !*element->str)
+		return (0);
+	if ((rest = ft_strchr(element->str, '\n')))
 		*rest = '\0';
 	if (!(*line = ft_strdup(element->str)))
 		return (-1);
-	if (rest)
-	{
-		if (!(element->str = ft_strdup(rest + 1)))
-			return (-1);
-	}
+	if (!rest)
+		ft_strdel(&element->str);
 	else
-		return (0);
+		element->str = ft_strdup(rest + 1);
 	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static	t_data	*element;
+	t_data			*element;
+	static t_data	*head;
 	char			*buf;
 	int				rd;
 
-	element = ft_check_fd(fd, element);
-	if (fd < 0 || !line || !(buf = ft_strnew(BUFF_SIZE)))
+	if (fd < 0 || !line || !(buf = ft_strnew(BUFF_SIZE)) ||
+								(!head && (!(head = ft_new_list(fd)))))
 		return (-1);
+	element = ft_check_fd(fd, head);
 	while ((rd = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		(element->str = ft_strjoinf(&(element->str),
-									&buf, element->str ? 1 : 0));
+		element->str = ft_strjoinf(&(element->str), &buf, 1);
+		ft_strclr(buf);
 		if (ft_strchr(element->str, '\n'))
 			break ;
 	}
 	ft_strdel(&buf);
-	return (ft_storage(element, line));
+	return (rd >= 0 ? ft_storage(element, line) : -1);
 }
